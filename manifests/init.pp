c3705ca3b14291b6f01811b9ac64140bc09875ea
Daniel Vigueras <daniel@scalia.es>
2013-01-13 19:59:22 +0100

    Fixed bug in activate_service param

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,7 @@
 class sphinx (
-  $package_name     = $sphinx::params::sphinx_package_name,
-  $package_ensure   = 'present',
-  $service_name     = $sphinx::params::sphinx_service_name,
-  $activate_service = $sphinx::params::sphinx_activate_service
+  $package_name   = $sphinx::params::sphinx_package_name,
+  $package_ensure = 'present',
+  $service_name   = $sphinx::params::sphinx_service_name,
 ) inherits sphinx::params {
 
   class { 'sphinx::package':
@@ -12,13 +11,13 @@ class sphinx (
   }
 
   class { 'sphinx::config':
+    activate_service => $sphinx::params::sphinx_activate_service,
     require => Class['sphinx::package'],
     notify  => Class['sphinx::service'],
   }
 
   class { 'sphinx::service':
     service_name     => $service_name,
-    activate_service => $activate_service
   }
 
 }
